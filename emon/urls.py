af9bbf2fbe172d5b298f2a2065a309ed15fcac45
Nelis <nelisvolschenk@gmail.com>
2017-12-03 17:44:42 +0200

    Initial Commit

--- emon/urls.py
+++ emon/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.Index.as_view(), name='index'),
     url(r'^installations/$', views.InstallationListView.as_view(), name='installations'),
     url(r'^installation/(?P<pk>\d+)/$', views.InstallationDetailView.as_view(), name='installation-detail'),
     url(r'^meters/$', views.MeterListView.as_view(), name='meters'),
--- emon/views.py
+++ emon/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+#from django.views.generic.base import TemplateView
+from django.views import generic
+from .models import Installation, Meter, Channel, ChannelData
 
 # Create your views here.
+
+class Index(List):
+    template_name = "index.html"
+
+
+class InstallationListView():
